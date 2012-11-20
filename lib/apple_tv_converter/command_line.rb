2dbfee70feaf33ce7697587c33527fc21ca51da2
Pedro Rodrigues <pedro.rodrigues@cldware.com>
2012-11-20 23:39:11 +0000

    Added progress information to the command line utility

--- lib/apple_tv_converter/command_line.rb
+++ lib/apple_tv_converter/command_line.rb
@@ -12,7 +12,8 @@ module AppleTvConverter
 
         converter = AppleTvConverter::MediaConverter.new(options)
 
-        media_objects.each do |media|
+        media_objects.each_with_index do |media, index|
+          puts "---[ Processing file #{index + 1} of #{media_objects.length}: #{File.basename(media.original_filename)} ]----------------"
           converter.process_media media
         end
       rescue ArgumentError => e
