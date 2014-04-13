6fb052b720ce7c221c34eba24e5ca1ae00093158
Ildar Shaynurov <shaynurov@gmail.com>
2014-04-13 15:22:18 +0400

    Format parser: refactor

--- lib/apple_tv_converter/filename_parser.rb
+++ lib/apple_tv_converter/filename_parser.rb
@@ -24,6 +24,20 @@ module AppleTvConverter
 
     private
 
+    def basename
+      @basename ||= File.basename(@path)
+    end
+
+    def format1_match
+      # /.*?S(\d+)E(\d+)(?:(?:[-E]+(\d+))*).*?/ -> S00E01, S00E01(E02)+, S00E01(-E02)+, S00E01(-02)+
+      @format1_match ||= basename.match(/.*?S(\d+)E(\d+)(?:(?:[-E]+(\d+))*).*?/i)
+    end
+
+    def format2_match
+      # /(\d+)x(\d+)(?:(?:_?(?:\1)x(\d+))*)/ -> 0x01, 0x01(_0x02)+ , assuming same season number (0x01_1x02 fails!)
+      @format2_match ||= basename.match(/(\d+)x(\d+)(?:(?:_?(?:\1)x(\d+))*)/i)
+    end
+
     def parse_tvshow_name
       test_path = File.expand_path(File.basename(File.dirname(@path)) =~ /^season\s*\d+/i ? File.dirname(File.dirname(@path)) : File.dirname(@path))
       match = test_path.match(/.*\/(.*?)(?:S(\d+))?$/i)
@@ -31,33 +45,18 @@ module AppleTvConverter
     end
 
     def parse_season_number
-      # /.*?S(\d+)E(\d+)(?:(?:[-E]+(\d+))*).*?/ -> S00E01, S00E01(E02)+, S00E01(-E02)+, S00E01(-02)+
-      match = File.basename(@path).match(/.*?S(\d+)E(\d+)(?:(?:[-E]+(\d+))*).*?/i)
-      return match[1].to_i if match
-
-      # /(\d+)x(\d+)(?:(?:_?(?:\1)x(\d+))*)/ -> 0x01, 0x01(_0x02)+ , assuming same season number (0x01_1x02 fails!)
-      match = File.basename(@path).match(/(\d+)x(\d+)(?:(?:_?(?:\1)x(\d+))*)/i)
-      match[1].to_i if match
+      return format1_match[1].to_i if format1_match
+      return format2_match[1].to_i if format2_match
     end
 
     def parse_episode_number
-      # /.*?S(\d+)E(\d+)(?:(?:[-E]+(\d+))*).*?/ -> S00E01, S00E01(E02)+, S00E01(-E02)+, S00E01(-02)+
-      match = File.basename(@path).match(/.*?S(\d+)E(\d+)(?:(?:[-E]+(\d+))*).*?/i)
-      return match[2].to_i if match
-
-      # /(\d+)x(\d+)(?:(?:_?(?:\1)x(\d+))*)/ -> 0x01, 0x01(_0x02)+ , assuming same season number (0x01_1x02 fails!)
-      match = File.basename(@path).match(/(\d+)x(\d+)(?:(?:_?(?:\1)x(\d+))*)/i)
-      match[2].to_i if match
+      return format1_match[2].to_i if format1_match
+      return format2_match[2].to_i if format2_match
     end
 
     def parse_last_episode_number
-      # /.*?S(\d+)E(\d+)(?:(?:[-E]+(\d+))*).*?/ -> S00E01, S00E01(E02)+, S00E01(-E02)+, S00E01(-02)+
-      match = File.basename(@path).match(/.*?S(\d+)E(\d+)(?:(?:[-E]+(\d+))*).*?/i)
-      return match[3].to_i if match && match[3]
-
-      # /(\d+)x(\d+)(?:(?:_?(?:\1)x(\d+))*)/ -> 0x01, 0x01(_0x02)+ , assuming same season number (0x01_1x02 fails!)
-      match = File.basename(@path).match(/(\d+)x(\d+)(?:(?:_?(?:\1)x(\d+))*)/i)
-      return match[3].to_i if match && match[3]
+      return format1_match[3].to_i if format1_match && format1_match[3]
+      return format2_match[3].to_i if format2_match && format2_match[3]
     end
   end
 end
